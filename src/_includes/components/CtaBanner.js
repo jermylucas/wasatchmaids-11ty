c97c328db18d11927d24016953ff6bcb4d197ca9
Jeremy Lucas <47125917+jermylucas@users.noreply.github.com>
2023-01-22 10:54:01 -0700

    Setting up arialabel stuffs

--- src/_includes/components/CtaBanner.js
+++ src/_includes/components/CtaBanner.js
@@ -1,4 +1,4 @@
-const { html } = require("common-tags");
+const { html } = require('common-tags');
 
 function CtaBanner({ text, btnText, btnLink, dark }) {
   return html`
@@ -7,7 +7,7 @@ function CtaBanner({ text, btnText, btnLink, dark }) {
         <div class="cta-grid">
           <h4 class="cta-grid-text">${text}</h4>
           <div class="cta-grid-btn">
-            <a class="button-solid-small" href="${btnLink}">${btnText}</a>
+            <a class="button-solid-small" href="${btnLink}" aria-label="Book with us">${btnText}</a>
           </div>
         </div>
       </div>
