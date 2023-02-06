cc0a34f4c24dd8d62a6f05bd87b9d47715288150
Jeremy Lucas <jplucas14@gmail.com>
2023-02-06 16:46:38 -0700

    Pulled first and also added passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,32 +1,33 @@
-const { DateTime } = require("luxon");
-const CtaBanner = require("./src/_includes/components/CtaBanner");
-const AddToContactsBtn = require("./src/_includes/components/AddToContactsBtn");
+const { DateTime } = require('luxon');
+const CtaBanner = require('./src/_includes/components/CtaBanner');
+const AddToContactsBtn = require('./src/_includes/components/AddToContactsBtn');
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets/");
-  eleventyConfig.addPassthroughCopy("src/css/");
-  eleventyConfig.addPassthroughCopy("src/fonts/");
-  eleventyConfig.addPassthroughCopy("src/scripts/");
-  eleventyConfig.addPassthroughCopy("src/admin/");
+  eleventyConfig.addPassthroughCopy('src/assets/');
+  eleventyConfig.addPassthroughCopy('src/css/');
+  eleventyConfig.addPassthroughCopy('src/fonts/');
+  eleventyConfig.addPassthroughCopy('src/scripts/');
+  eleventyConfig.addPassthroughCopy('src/admin/');
+  eleventyConfig.addPassthroughCopy('src/_redirects');
 
-  eleventyConfig.addWatchTarget("src/**/*.css");
+  eleventyConfig.addWatchTarget('src/**/*.css');
 
-  eleventyConfig.addShortcode("CtaBanner", CtaBanner);
-  eleventyConfig.addShortcode("AddToContactsBtn", AddToContactsBtn);
+  eleventyConfig.addShortcode('CtaBanner', CtaBanner);
+  eleventyConfig.addShortcode('AddToContactsBtn', AddToContactsBtn);
 
-  eleventyConfig.addFilter("postDate", (dateObj) => {
+  eleventyConfig.addFilter('postDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
   return {
     dir: {
-      input: "src",
-      includes: "_includes",
-      output: "_site",
+      input: 'src',
+      includes: '_includes',
+      output: '_site',
     },
-    templateFormts: ["md", "njk", "html"],
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
+    templateFormts: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
   };
 };
